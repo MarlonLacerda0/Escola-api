d4e5a89d3d9fa7b8ddb35576c3e38544cfcfb0a8
Marlon <marlonlac10@hotmail.com>
2026-04-18 13:43:39 -0300

    Adicionar arquivos de projeto.

---
+++ Escola.Domain/Interfaces/ITurmaRepository.cs
@@ -0,0 +1,16 @@
+﻿using Escola.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola.Domain.Interfaces
+{
+    public interface ITurmaRepository
+    {
+        Task<Turma> GetByIdAsync(Guid id);
+        Task<List<Turma>> GetAllAsync();
+        Task<Turma> AddAsync(Turma turma);
+        Task<Turma> UpdateAsync(Turma turma);
+        Task<Turma> DeleteAsync(int id);
+    }
+}
---
+++ Escola.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Escola.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Curso> Curso { get; set; }
+        public DbSet<Turma> Turma { get; set; }
+        public DbSet<Matricula> Matricula { get; set; }
+        public DbSet<Nota> Nota { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
