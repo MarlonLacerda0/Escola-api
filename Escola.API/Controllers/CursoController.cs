04e4945bcddd093ff10f4312d3eeab7ab36131ef
Marlon <marlonlac10@hotmail.com>
2026-04-21 20:54:21 -0300

    Criando mais controllers

---
+++ Escola.API/Controllers/CursoController.cs
@@ -0,0 +1,64 @@
+﻿using Escola.Application.DTOs.Curso;
+using Escola.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escola.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CursoController : Controller
+    {
+        private readonly ICursoService _cursoService;
+        public CursoController(ICursoService cursoService)
+        {
+            _cursoService = cursoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCursos()
+        {
+            var cursos = await _cursoService.GetAllAsync();
+            return Ok(cursos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCursoById(int id)
+        {
+            var curso = await _cursoService.GetByIdAsync(id);
+            if (curso == null)
+                return NotFound("Curso não encontrado.");
+
+            return Ok(curso);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCurso(int id)
+        {
+            var deletedCurso = await _cursoService.DeleteAsync(id);
+            if (deletedCurso == null)
+                return BadRequest("Curso não encontrado para exclusão.");
+
+            return Ok(new { message = "Curso excluído com Sucesso" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCurso(CursoPostDTO cursoPostDTO)
+        {
+            var createdCurso = await _cursoService.AddAsync(cursoPostDTO);
+            if (createdCurso == null)
+                return BadRequest("Não foi possível criar o curso.");
+
+            return Ok(new { message = "Curso incluído com Sucesso" });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCurso(CursoPutDTO cursoPutDTO)
+        {
+            var updatedCurso = await _cursoService.UpdateAsync(cursoPutDTO);
+            if (updatedCurso == null)
+                return BadRequest("Curso não encontrado para atualização.");
+            return Ok(new { message = "Curso atualizado com Sucesso" });
+        }
+
+    }
+}
---
+++ Escola.API/Controllers/MatriculaController.cs
@@ -0,0 +1,60 @@
+﻿using Escola.Application.DTOs.Matricula;
+using Escola.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Escola.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MatriculaController : Controller
+    {
+        private readonly IMatriculaService _matriculaService;
+        public MatriculaController(IMatriculaService matriculaService)
+        {
+            _matriculaService = matriculaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMatriculas()
+        {
+            var matriculas = await _matriculaService.GetAllAsync();
+            return Ok(matriculas);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMatriculaById(int id)
+        {
+            var matricula = await _matriculaService.GetByIdAsync(id);
+            if (matricula == null)
+                return NotFound("Matrícula não encontrada.");
+            return Ok(matricula);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMatricula(int id)
+        {
+            var deletedMatricula = await _matriculaService.DeleteAsync(id);
+            if (deletedMatricula == null)
+                return BadRequest("Matrícula não encontrada para exclusão.");
+            return Ok(new { message = "Matrícula excluída com Sucesso" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMatricula(MatriculaPostDTO matriculaPostDTO)
+        {
+            var createdMatricula = await _matriculaService.AddAsync(matriculaPostDTO);
+            if (createdMatricula == null)
+                return BadRequest("Não foi possível criar a matrícula.");
+            return Ok(new { message = "Matrícula incluída com Sucesso" });
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMatricula(MatriculaPutDTO matriculaPutDTO)
+        {
+            var updatedMatricula = await _matriculaService.UpdateAsync(matriculaPutDTO);
+            if (updatedMatricula == null)
+                return BadRequest("Matrícula não encontrada para atualização.");
+            return Ok(new { message = "Matrícula atualizada com Sucesso" });
+        }
+    }
+}
